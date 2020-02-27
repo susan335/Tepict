942928ff67e30485c219a23ac6b50eb119bbcba1
watanave <susan.se3p@gmail.com>
2020-02-27 22:49:18 +0900

    Add Preference ui

--- Tepict/PreferenceView.swift
+++ Tepict/PreferenceView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct PreferenceView: View {
+    @ObservedObject var settings = UserData()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Picker(selection: $settings.previewLocation, label: Text("Preview Location")) {
+                ForEach([
+                    PreviewLocation.full,
+                    PreviewLocation.left,
+                    PreviewLocation.top,
+                    PreviewLocation.right,
+                    PreviewLocation.bottom
+                ]) { location in
+                    Text(location.rawValue.uppercased()).tag(location)
+                }
+            }
+            .pickerStyle(RadioGroupPickerStyle())
+            
+            Spacer().frame(height: 40)
+            
+//            HStack {
+//                Text("Left offset: ")
+//                    .frame(width: 100, alignment: .trailing)
+//                TextField("0px", text: $settings.leftOffset)
+//                    .frame(width: 100, alignment: .leading)
+//            }
+//            HStack {
+//                Text("Top offset: ")
+//                    .frame(width: 100, alignment: .trailing)
+//                TextField("0px", text: $leftOffset)
+//                    .frame(width: 100, alignment: .leading)
+//            }
+//            HStack {
+//                Text("Right offset: ")
+//                    .frame(width: 100, alignment: .trailing)
+//                TextField("0px", text: $leftOffset)
+//                    .frame(width: 100, alignment: .leading)
+//            }
+//            HStack {
+//                Text("Bottom offset: ")
+//                    .frame(width: 100, alignment: .trailing)
+//                TextField("0px", text: $leftOffset)
+//                    .frame(width: 100, alignment: .leading)
+//            }
+        }
+        .frame(width: 300, height: 400, alignment: .center)
     }
 }
 
